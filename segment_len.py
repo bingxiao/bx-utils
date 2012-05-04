06f171d1c0cdea649349cc04e36be6790ec4f419
bingxiao <alberthwc2003@gmail.com>
2012-05-04 12:54:32 +0800

    Adding: output_dir as an extra argument

--- segment_len.py
+++ segment_len.py
@@ -6,17 +6,24 @@ import pysam
 import sys
 import os
 
+def usage():
+    print >>sys.stdout, 'Usage: %s <bam_file.bam> <output_directory>'%sys.argv[0]
+
 try:
     input_file = sys.argv[1]
-    prefix = os.path.split( input_file )[-1]
+    prefix = os.path.basename( input_file )
     if not os.path.exists( input_file + '.bai' ):
         print >>sys.stderr, '.bai file missing, you have to index the bam file first:\n# samtools index %s'%input_file
         sys.exit()
     #samfile = pysam.Samfile( 'a.bam', 'rb' )
     samfile = pysam.Samfile( input_file, 'rb' )
 except:
-    print 'usage: %s <bam_file.bam>'%sys.argv[0]
+    usage()
     exit(0)
+
+out_dir = sys.argv[2]
+if not os.path.exists( out_dir ):
+    os.mkdir( out_dir )
 
 def get_seg_len( read1, read2 ):
     return max( read1.aend, read2.aend ) - min( read1.pos, read2.pos )
@@ -25,7 +32,7 @@ read1_dic = {}
 read2_dic = {}
 
 segment_length_lst = []
-with open( prefix+'.seglen.all', 'w' ) as out:
+with open( os.path.join( out_dir, prefix+'.seglen.all'), 'w' ) as out:
     for n, read in enumerate( samfile.fetch() ): 
         # if read.is_read1 and
         if read.is_proper_pair and not read.is_qcfail:
@@ -47,8 +54,9 @@ with open( prefix+'.seglen.all', 'w' ) as out:
                 else:
                     read2_dic[ read.qname ] = read
         if n % 100000 == 0:
-            print >>sys.stderr, 'Parsed %d reads.\r'%n,
+            print >>sys.stderr, '\rParsing %d reads.'%n,
 
+print >>sys.stderr, "\nOutput file:", out.name, "writen."
 samfile.close()
 
 segment_length_lst = np.array( segment_length_lst )
@@ -65,14 +73,20 @@ with open( out.name ) as handler:
     a = [  line.strip().split('\t') for line in handler ]
 
 #from collections import Counter
+print >>sys.stderr, "Preparing unique file..."
+
 cnt = dict()
 for i in zip( *a )[0]:
-    if i not in cnt: cnt[ i ] = 0
-    cnt[ i ] += 1
+    if i not in cnt: 
+        cnt[ i ] = 1
+    else:
+        cnt[ i ] += 1
 
-with open( prefix + '.seglen.uniq', 'w' ) as out1:
+print >>sys.stderr, "Output file:", out.name, "writing.."
+with open( os.path.join( out_dir, prefix+'.seglen.uniq' ), 'w' ) as out1:
     for b in a:
         if cnt[ b[0] ] > 1: continue
         print >>out1, '\t'.join( b )
+print >>sys.stderr, "Done."
 
 
